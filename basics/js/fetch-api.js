138beb759ddf9cc50105e260ab77e60cf0255b1e
Andrew0914 <andrewalangm@gmail.com>
2021-03-29 15:45:15 -0600

    🎇 Creating Request object

--- basics/js/fetch-api.js
+++ basics/js/fetch-api.js
@@ -16,17 +16,18 @@ try {
 // Advance use of fetch api
 
 async function savePost({ title, body, userId }) {
+  const savePostRequest = new Request(
+    "https://jsonplaceholder.typicode.com/postxs",
+    {
+      method: "POST",
+      body: JSON.stringify({ title, body, userId }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
   try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/postxs",
-      {
-        method: "POST",
-        body: JSON.stringify({ title, body, userId }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    );
+    const response = await fetch(savePostRequest);
 
     const data = await response.json();
 
