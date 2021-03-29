6c65eea40b7254353f7bba25464fca6346680668
Andrew0914 <andrewalangm@gmail.com>
2021-03-29 15:44:12 -0600

    🌐 Making a post request with more details

--- basics/js/fetch-api.js
+++ basics/js/fetch-api.js
@@ -12,3 +12,26 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+// Advance use of fetch api
+
+async function savePost({ title, body, userId }) {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/postxs",
+      {
+        method: "POST",
+        body: JSON.stringify({ title, body, userId }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    console.log("✔ Post saved", data);
+  } catch (error) {
+    console.error(error);
+  }
+}
