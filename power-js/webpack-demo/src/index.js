60fa598ca9abf83041c19d8305aa7af6e69162f0
Andrew0914 <andrewalangm@gmail.com>
2021-04-07 22:03:05 -0500

    ⚙ Adding custom  configuration to webpack

--- power-js/webpack-demo/src/index.js
+++ power-js/webpack-demo/src/index.js
@@ -1,9 +1,12 @@
 import { join } from "lodash";
+import "../app.scss";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = join(["Hello", "webpack"], " ");
+  element.classList.add("title_hello");
 
   return element;
 }
