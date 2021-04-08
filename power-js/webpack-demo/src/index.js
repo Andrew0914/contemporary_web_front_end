6a2200b5a195e1271befc77bcb54cc7a57837b69
Andrew0914 <andrewalangm@gmail.com>
2021-04-07 21:48:52 -0500

    🧰 Genrating our bundlw with dependendcies

--- power-js/webpack-demo/src/index.js
+++ power-js/webpack-demo/src/index.js
@@ -1,8 +1,9 @@
+import { join } from "lodash";
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hello webpack"
+  element.innerHTML = join(["Hello", "webpack"], " ");
 
   return element;
 }
