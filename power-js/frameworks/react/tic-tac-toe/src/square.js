bf4da246c40dbca226da0e204cdbcbcedf4ebcee
Andrew0914 <andrewalangm@gmail.com>
2021-04-16 21:42:50 -0500

    🥞 Keeping state with React for each square

--- power-js/frameworks/react/tic-tac-toe/src/square.js
+++ power-js/frameworks/react/tic-tac-toe/src/square.js
@@ -1,6 +1,21 @@
 import React from "react";
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  mark() {
+    this.setState({ value: "X" });
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.mark()}>
+        {this.state.value}
+      </button>
+    );
   }
 }
